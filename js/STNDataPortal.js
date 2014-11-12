c061a208ab933769cb2b75487cf90023892cf027
BlakeDraper <bdraper@usgs.gov>
2014-11-12 16:21:37 -0600

    added some of the ajax calls to populate dropdowns

--- js/STNDataPortal.js
+++ js/STNDataPortal.js
@@ -1,34 +1,89 @@
 /**
  * Created by bdraper on 11/10/2014.
  */
-
-
-$( document ).ready(function() {
-    alert( "ready!" );
+$(document).ready(function () {
 
     $.ajax({
         dataType: 'xml',
         type: 'GET',
-        url:'http://107.20.206.65/STNServices/Statustypes',
-        headers:{
-            'Accept': '*/*'
+        url: 'http://107.20.206.65/STNServices/statustypes',
+        headers:{'Accept': '*/*'},
+        success: function (xml) {
+            $(xml).find('STATUS_TYPE').each(function () {
+                var status = $(this).find('STATUS').text();
+                $('#stateSelect').append("<option value='" + status + "'>" + status + "</option>");
+            });
         },
-        success: function(data, status, jqXHR){
-            if (data.length>0){
-                $('#stateSelect').append($('<option></option>').val(0).html("All"));
-                $.each(data, function(){
-
-                    alert("this ajax is returning:" + data);
-                    //$('#stateSelect').append($('<option></option>').val(this['STATE']).html(this['STATE']));
-                });
-            }
-        },
-        error: function(error) {
-            alert("Oh snap, error. Here it is: " + error);
+        error: function (error) {
+           console.log("Error processing the XML. The error is:" + error);
         }
     });
-});
 
+    $.ajax({
+        dataType: 'xml',
+        type: 'GET',
+        url: 'http://107.20.206.65/STNServices/events',
+        headers: {'Accept': '*/*'},
+        success: function (xml) {
+            $(xml).find('EVENT').each(function () {
+                var eventName = $(this).find('EVENT_NAME').text();
+                $('#evtSelect').append("<option value='" + eventName + "'>" + eventName + "</option>");
+            });
+        },
+        error: function (error) {
+            console.log("Error processing the XML. The error is:" + error);
+        }
+    });
+
+    $.ajax({
+        dataType: 'xml',
+        type: 'GET',
+        url: 'http://107.20.206.65/STNServices/deploymenttypes',
+        headers: {'Accept': '*/*'},
+        success: function (xml) {
+            $(xml).find('DEPLOYMENT_TYPE').each(function () {
+                var deployType = $(this).find('METHOD').text();
+                $('#deployTypeSelect').append("<option value='" + deployType + "'>" + deployType + "</option>");
+            });
+        },
+        error: function (error) {
+            console.log("Error processing the XML. The error is:" + error);
+        }
+    });
+
+    $.ajax({
+        dataType: 'xml',
+        type: 'GET',
+        url: 'http://107.20.206.65/STNServices/eventtypes',
+        headers: {'Accept': '*/*'},
+        success: function (xml) {
+            $(xml).find('EVENT_TYPE').each(function () {
+                var eventType = $(this).find('TYPE').text();
+                $('#eventTypeSelect').append("<option value='" + eventType + "'>" + eventType + "</option>");
+            });
+        },
+        error: function (error) {
+            console.log("Error processing the XML. The error is:" + error);
+        }
+    });
+
+    $.ajax({
+        dataType: 'xml',
+        type: 'GET',
+        url: 'http://107.20.206.65/STNServices/housingtypes',
+        headers: {'Accept': '*/*'},
+        success: function (xml) {
+            $(xml).find('HOUSING_TYPE').each(function () {
+                var housingType = $(this).find('TYPE_NAME').text();
+                $('#housingTypeSelect').append("<option value='" + housingType + "'>" + housingType + "</option>");
+            });
+        },
+        error: function (error) {
+            console.log("Error processing the XML. The error is:" + error);
+        }
+    });
+
+});
 
 
 
